e4107a240cc18e99a755206e1d3f00373e4d066d
横天 <xuejia.cxj@taobao.com>
2014-07-26 15:41:13 +0800

    初始化项目

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+	less = require('gulp-less');
+    // watch = require('gulp-watch');
+
+
+// gulp.task('lessc', function () {
+// 	console.log('start lessc:');
+//     return ;
+// });
+
+console.log('I am restarted');
+gulp.src(['src/less/*.less'])
+	.pipe(less())    		
+    .pipe(gulp.dest('css/'));
+
+// gulp.task('default', ['lessc']);
